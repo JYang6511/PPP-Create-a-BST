f1a0b370630a2a51dec1757ab8b7d43750e11155
Johnson Yang <johnsonyang65@gmail.com>
2024-02-21 21:41:35 -0800

    step2

--- BST.py
+++ BST.py
@@ -63,7 +63,32 @@ print(node3) #5
 """
 Part 2: Create a BST class
 """
-
+class BST:
+  def __init__(self, root=None):
+    self.root = root
+    self.contents = []
+  
+  def __str__(self):
+    if self.root == None:
+      return "The tree is empty"
+    else:
+      self.output = ''
+      self.print_tree(node=self.root)
+      return self.output
+  
+  def __repr__(self):
+    if self.root == None:
+      return "The tree is empty"
+    else:
+      self.output = ''
+      self.print_tree(node=self.root)
+      return self.output
+  
+  def print_tree(self, node, level=0):
+    if node != None:
+      self.print_tree(node.right, level + 1)
+      self.output += ' ' * 4 * level + '-> ' + str(node.data) + '\n'
+      self.print_tree(node.left, level + 1)
 
 
 
