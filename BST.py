f4a63a6b35b24af16a531e9eaae5ede10b1f40e4
Johnson Yang <johnsonyang65@gmail.com>
2024-02-21 21:30:20 -0800

    part 1

--- BST.py
+++ BST.py
@@ -36,8 +36,29 @@ node3.data = 5
 print(node3) #5
 """
 
+class BSTNode:
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.data)
+
+    def __repr__(self):
+        return str(self.data)
 
 
+node1 = BSTNode(3)
+print(node1) #3
+
+node2 = BSTNode(4, left=node1)
+print(node2) #4
+
+node3 = BSTNode()
+print(node3) #None
+node3.data = 5
+print(node3) #5
 
 """
 Part 2: Create a BST class
